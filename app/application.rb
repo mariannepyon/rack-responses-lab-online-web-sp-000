2e89c03133822a592e0fa987249ca05039041a17
Marianne Pyon <mariannepyon@gmail.com>
2019-10-21 17:19:14 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,3 +1,24 @@
+# class Application
+#
+#   def call(env)
+#     resp = Rack::Response.new
+#
+#     num_1 = Kernel.rand(1..11)
+#     num_2 = Kernel.rand(12..24)
+#
+#     resp.write "#{num_1}\n"
+#     resp.write "#{"num_2"}\n"
+#
+#     if num_1
+#       resp.write "Good Morning!"
+#     else
+#       resp.write "Good Afternoon!"
+#     end
+#
+#     resp.finish
+#   end
+#
+# end
 class Application
 
   def call(env)
